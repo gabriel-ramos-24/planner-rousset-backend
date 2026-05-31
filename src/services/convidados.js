dafb3cd750b0c137ca8466f73cf5d88d1dc646f1
gabriel-ramos-24 <gabriel.ramos.1567380@sga.pucminas.br>
2026-05-31 03:09:18 -0300

    Padrão de retorno JSON

--- src/services/convidados.js
+++ src/services/convidados.js
@@ -8,12 +8,12 @@ export async function getInfoConvidados(env, tokenConvidado) {
 
     if (!payload) return { body: { mensagem: "Token inválido!" }, status: 403 };
 
-    if (!payload.contratoId) return { mensagem: "Token válido, mas sem as informações necessárias", status: 400 }
+    if (!payload.contratoId) return { body: { mensagem: "Token válido, mas sem as informações necessárias" }, status: 400 }
 
     try {
 
         const lancamentos = await env.DB.prepare("SELECT l.id, l.data, p.nome, l.quantidade, p.valor_unitario, l.quantidade * p.valor_unitario AS valor_total FROM lancamentos l INNER JOIN produtos p ON p.id = l.produto_id WHERE p.contrato_id = ? ORDER BY l.data ASC").bind(payload.contratoId).all();
-        return { dados: lancamentos.results, mensagem: "Consulta realizada com sucesso!", status: 200 };
+        return { body: { dados: lancamentos.results, mensagem: "Consulta realizada com sucesso!" }, status: 200 };
 
     } catch (error) {
         console.error("Log de erro dentro da pasta database ao buscar todos os lancamentos: ", error);
