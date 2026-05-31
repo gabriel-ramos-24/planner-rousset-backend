ab86265c8e3ff42b3f34114a6a2da7e4c74baa25
gabriel-ramos-24 <gabriel.ramos.1567380@sga.pucminas.br>
2026-05-31 01:11:08 -0300

    Update CORS headers to include Authorization in allowed headers

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { validarToken } from './services/autenticacao.js';
 const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-  "Access-Control-Allow-Headers": "Content-Type"
+  "Access-Control-Allow-Headers": "Content-Type, Authorization"
 };
 
 const routes = [
